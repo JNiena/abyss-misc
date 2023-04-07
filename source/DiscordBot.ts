7769ff3a2df7082bf886b554a011c326ef902969
Jack Nienaber <JNiena@protonmail.com>
2023-04-07 17:26:46 -0400

    Now reads directly from config

--- source/DiscordBot.ts
+++ source/DiscordBot.ts
@@ -1,21 +1,20 @@
-import { Config } from "./Config";
 import { AkairoClient, Command, CommandHandler, Inhibitor, InhibitorHandler, Listener, ListenerHandler } from "discord-akairo";
-import { TextChannel } from "discord.js";
+import { MessageEmbed, TextChannel } from "discord.js";
+import Config from "./Config";
 
 export class DiscordBot extends AkairoClient {
-
 	private commandHandler: CommandHandler;
 	private inhibitorHandler: InhibitorHandler;
 	private listenerHandler: ListenerHandler;
 
-	public constructor(config: Config) {
+	public constructor() {
 		super();
-		this.commandHandler = new CommandHandler(this, { "prefix": config.get()["discord"]["prefix"] });
+		this.commandHandler = new CommandHandler(this, {});
 		this.inhibitorHandler = new InhibitorHandler(this, {});
 		this.listenerHandler = new ListenerHandler(this, {});
 		this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
 		this.commandHandler.useListenerHandler(this.listenerHandler);
-		this.token = config.get()["discord"]["token"];
+		this.token = Config.token;
 	}
 
 	public start(): void {
@@ -40,11 +39,14 @@ export class DiscordBot extends AkairoClient {
 		this.listenerHandler.register(listener);
 	}
 
-	public async send(message: string, channelID: string): Promise<any> {
+	public async send(message: string): Promise<any> {
 		if (message.trim().length === 0) {
 			return Promise.resolve();
 		}
-		await (this.channels.cache.get(channelID) as TextChannel).send(message);
+		await (this.channels.cache.get(Config.channelID) as TextChannel).send(message);
 	}
 
+	public async sendEmbed(embed: MessageEmbed): Promise<void> {
+		await (this.channels.cache.get(Config.channelID) as TextChannel).send(embed);
+	}
 }
